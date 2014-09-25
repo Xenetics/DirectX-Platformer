d029eed706af2d16815d8e7144c2cc2b145315d0
DamianKutsch <kutsch.damian@gmail.com>
2014-09-24 23:03:22 -0400

    Menu stuff
    Added functions to try and get the menu's working...not working yet...

--- MeshView/MeshViewDemo.cpp
+++ MeshView/MeshViewDemo.cpp
@@ -37,6 +37,12 @@ struct BoundingSphere
 	XMFLOAT3 Center;
 	float Radius;
 };
+
+struct Vertexes //used to make quad vertices for the menu, do not touch please (Damian)
+{
+	XMFLOAT3 Pos;
+	XMFLOAT4 Color;
+};
  
 class MeshViewApp : public D3DApp 
 {
@@ -68,11 +74,16 @@ private:
 	void DrawScreenQuad(ID3D11ShaderResourceView* srv);
 	void BuildShadowTransform();
 	void BuildScreenQuadGeometryBuffers();
+	void BuildGeometryBuffers();
+	void BuildMenuFX();
+	void BuildVertexLayout();
 
-	//states
+
+	//state based functions
 	void UpdateWhilePlaying(float dt);
+	void UpdateMainMenu(float dt);
 	void DrawWhilePlaying();
-	void DrawMenuScene();
+	void DrawMenu();
 
 	//misc functions
 	void KeyHandler(float dt);
@@ -89,6 +100,17 @@ private:
 
 	std::vector<BasicModelInstance> mModelInstances;
 	std::vector<BasicModelInstance> mAlphaClippedModelInstances;
+
+	//stuff bellow is needed for my menus (Damian)
+	ID3D11Buffer* mBoxVB;
+	ID3D11Buffer* mBoxIB;
+	ID3DX11Effect* mFX;
+	ID3DX11EffectTechnique* mTech;
+	ID3DX11EffectMatrixVariable* mfxWorldViewProj;
+	ID3D11InputLayout* mInputLayout;
+	XMFLOAT4X4 mWorld;
+	XMFLOAT4X4 mView;
+	XMFLOAT4X4 mProj;
 
 	ID3D11Buffer* mSkySphereVB;
 	ID3D11Buffer* mSkySphereIB;
@@ -114,6 +136,11 @@ private:
 	SoundMgr* mSound;
 
 	POINT mLastMousePos;
+
+	//important menu floats
+	float mTheta;
+	float mPhi;
+	float mRadius;
 
 	//key bools
 	bool wKey = false;
@@ -144,7 +171,9 @@ MeshViewApp::MeshViewApp(HINSTANCE hInstance)
 : D3DApp(hInstance), mSky(0), testModel(0), currLevel(0),
 mScreenQuadVB(0), mScreenQuadIB(0),
   mSmap(0), mSsao(0),
-  mLightRotationAngle(0.0f)
+  mLightRotationAngle(0.0f), mBoxVB(0), mBoxIB(0), mFX(0), mTech(0),
+  mfxWorldViewProj(0), mInputLayout(0),
+  mTheta(1.5f*MathHelper::Pi), mPhi(0.25f*MathHelper::Pi), mRadius(5.0f)
 {
 	mMainWndCaption = L"Platformer";
 	
@@ -275,6 +304,11 @@ bool MeshViewApp::Init()
 	//init sound
 	mSound = new SoundMgr();
 
+	//build menu stuff
+	//BuildGeometryBuffers();
+	//BuildMenuFX();
+	//BuildVertexLayout();
+
 	return true;
 }
 
@@ -300,6 +334,7 @@ void MeshViewApp::UpdateScene(float dt)
 	switch (gameState)
 	{
 	case GAME_STATE::menuState:
+		UpdateMainMenu(dt);
 		break;
 
 	case GAME_STATE::playingState:
@@ -422,6 +457,22 @@ void MeshViewApp::UpdateWhilePlaying(float dt)
 	mPlayer.UpdateViewMatrix();
 }
 
+void MeshViewApp::UpdateMainMenu(float dt)
+{
+	// Convert Spherical to Cartesian coordinates.
+	float x = mRadius*sinf(mPhi)*cosf(mTheta);
+	float z = mRadius*sinf(mPhi)*sinf(mTheta);
+	float y = mRadius*cosf(mPhi);
+
+	// Build the view matrix.
+	XMVECTOR pos = XMVectorSet(x, y, z, 1.0f);
+	XMVECTOR target = XMVectorZero();
+	XMVECTOR up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+
+	XMMATRIX V = XMMatrixLookAtLH(pos, target, up);
+	XMStoreFloat4x4(&mView, V);
+}
+
 void MeshViewApp::DrawScene()
 {
 
@@ -430,6 +481,7 @@ void MeshViewApp::DrawScene()
 	switch (gameState)
 	{
 		case GAME_STATE::menuState:
+			DrawMenu();
 			break;
 
 		case GAME_STATE::playingState:
@@ -578,6 +630,40 @@ void MeshViewApp::DrawWhilePlaying()
 	// to it next frame.  These textures can be at any slot, so clear all slots.
 	ID3D11ShaderResourceView* nullSRV[16] = { 0 };
 	md3dImmediateContext->PSSetShaderResources(0, 16, nullSRV);
+
+	HR(mSwapChain->Present(0, 0));
+}
+
+void MeshViewApp::DrawMenu()
+{
+	md3dImmediateContext->ClearRenderTargetView(mRenderTargetView, reinterpret_cast<const float*>(&Colors::LightSteelBlue));
+	md3dImmediateContext->ClearDepthStencilView(mDepthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	md3dImmediateContext->IASetInputLayout(mInputLayout);
+	md3dImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	UINT stride = sizeof(Vertexes);
+	UINT offset = 0;
+	md3dImmediateContext->IASetVertexBuffers(0, 1, &mBoxVB, &stride, &offset);
+	md3dImmediateContext->IASetIndexBuffer(mBoxIB, DXGI_FORMAT_R32_UINT, 0);
+
+	// Set constants
+	XMMATRIX world = XMLoadFloat4x4(&mWorld);
+	XMMATRIX view = XMLoadFloat4x4(&mView);
+	XMMATRIX proj = XMLoadFloat4x4(&mProj);
+	XMMATRIX worldViewProj = world*view*proj;
+
+	mfxWorldViewProj->SetMatrix(reinterpret_cast<float*>(&worldViewProj));
+
+	D3DX11_TECHNIQUE_DESC techDesc;
+	mTech->GetDesc(&techDesc);
+	for (UINT p = 0; p < techDesc.Passes; ++p)
+	{
+		mTech->GetPassByIndex(p)->Apply(0, md3dImmediateContext);
+
+		// 36 indices for the box.
+		md3dImmediateContext->DrawIndexed(36, 0, 0);
+	}
 
 	HR(mSwapChain->Present(0, 0));
 }
@@ -1006,3 +1092,121 @@ void MeshViewApp::BuildScreenQuadGeometryBuffers()
     HR(md3dDevice->CreateBuffer(&ibd, &iinitData, &mScreenQuadIB));
 }
 
+void MeshViewApp::BuildGeometryBuffers()
+{
+	// Create vertex buffer
+	Vertexes vertices[] =
+	{
+		{ XMFLOAT3(-1.0f, -1.0f, -1.0f), (const float*)&Colors::White },
+		{ XMFLOAT3(-1.0f, +1.0f, -1.0f), (const float*)&Colors::Black },
+		{ XMFLOAT3(+1.0f, +1.0f, -1.0f), (const float*)&Colors::Red },
+		{ XMFLOAT3(+1.0f, -1.0f, -1.0f), (const float*)&Colors::Green },
+		{ XMFLOAT3(-1.0f, -1.0f, +1.0f), (const float*)&Colors::Blue },
+		{ XMFLOAT3(-1.0f, +1.0f, +1.0f), (const float*)&Colors::Yellow },
+		{ XMFLOAT3(+1.0f, +1.0f, +1.0f), (const float*)&Colors::Cyan },
+		{ XMFLOAT3(+1.0f, -1.0f, +1.0f), (const float*)&Colors::Magenta }
+	};
+
+	D3D11_BUFFER_DESC vbd;
+	vbd.Usage = D3D11_USAGE_IMMUTABLE;
+	vbd.ByteWidth = sizeof(Vertexes)* 8;
+	vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vbd.CPUAccessFlags = 0;
+	vbd.MiscFlags = 0;
+	vbd.StructureByteStride = 0;
+	D3D11_SUBRESOURCE_DATA vinitData;
+	vinitData.pSysMem = vertices;
+	HR(md3dDevice->CreateBuffer(&vbd, &vinitData, &mBoxVB));
+
+
+	// Create the index buffer
+
+	UINT indices[] = {
+		// front face
+		0, 1, 2,
+		0, 2, 3,
+
+		// back face
+		4, 6, 5,
+		4, 7, 6,
+
+		// left face
+		4, 5, 1,
+		4, 1, 0,
+
+		// right face
+		3, 2, 6,
+		3, 6, 7,
+
+		// top face
+		1, 5, 6,
+		1, 6, 2,
+
+		// bottom face
+		4, 0, 3,
+		4, 3, 7
+	};
+
+	D3D11_BUFFER_DESC ibd;
+	ibd.Usage = D3D11_USAGE_IMMUTABLE;
+	ibd.ByteWidth = sizeof(UINT)* 36;
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.CPUAccessFlags = 0;
+	ibd.MiscFlags = 0;
+	ibd.StructureByteStride = 0;
+	D3D11_SUBRESOURCE_DATA iinitData;
+	iinitData.pSysMem = indices;
+	HR(md3dDevice->CreateBuffer(&ibd, &iinitData, &mBoxIB));
+}
+
+void MeshViewApp::BuildMenuFX()
+{
+	DWORD shaderFlags = 0;
+#if defined( DEBUG ) || defined( _DEBUG )
+	shaderFlags |= D3D10_SHADER_DEBUG;
+	shaderFlags |= D3D10_SHADER_SKIP_OPTIMIZATION;
+#endif
+
+	ID3D10Blob* compiledShader = 0;
+	ID3D10Blob* compilationMsgs = 0;
+	HRESULT hr = D3DX11CompileFromFile(L"FX/color.fx", 0, 0, 0, "fx_5_0", shaderFlags,
+		0, 0, &compiledShader, &compilationMsgs, 0);
+
+	// compilationMsgs can store errors or warnings.
+	if (compilationMsgs != 0)
+	{
+		MessageBoxA(0, (char*)compilationMsgs->GetBufferPointer(), 0, 0);
+		ReleaseCOM(compilationMsgs);
+	}
+
+	// Even if there are no compilationMsgs, check to make sure there were no other errors.
+	if (FAILED(hr))
+	{
+		DXTrace(__FILE__, (DWORD)__LINE__, hr, L"D3DX11CompileFromFile", true);
+	}
+
+	HR(D3DX11CreateEffectFromMemory(compiledShader->GetBufferPointer(), compiledShader->GetBufferSize(),
+		0, md3dDevice, &mFX));
+
+	// Done with compiled shader.
+	ReleaseCOM(compiledShader);
+
+	mTech = mFX->GetTechniqueByName("ColorTech");
+	mfxWorldViewProj = mFX->GetVariableByName("gWorldViewProj")->AsMatrix();
+}
+
+void MeshViewApp::BuildVertexLayout()
+{
+	// Create the vertex input layout.
+	D3D11_INPUT_ELEMENT_DESC vertexDesc[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+	};
+
+	// Create the input layout
+	D3DX11_PASS_DESC passDesc;
+	mTech->GetPassByIndex(0)->GetDesc(&passDesc);
+	HR(md3dDevice->CreateInputLayout(vertexDesc, 2, passDesc.pIAInputSignature,
+		passDesc.IAInputSignatureSize, &mInputLayout));
+}
